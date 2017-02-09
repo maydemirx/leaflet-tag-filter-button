7c3aff2cedb43e1c542c88ba2c38be18e2d3c6cd
Mehmet Aydemir <mehmet.aydemir@ries.com.tr>
2017-02-09 17:23:38 +0300

    Demo updated
    added new features to demo

--- leaflet-tag-filter-button.js
+++ leaflet-tag-filter-button.js
@@ -7,6 +7,7 @@
             onSelectionComplete: null, // the callback function for selected tags
             data: null, // the data to be used for tags popup, it can be array or function
             clearText: 'clear', // the text of the clear button
+            filterOnEveryClick: false, // if set as true the plugin do filtering operation on every click event on the checkboxes
 
             ajaxData: null // it can be used for remote data TODO: implement it!
         },
@@ -15,6 +16,7 @@
         _container: null,
         _easyButton: null,
         _tagEl: null,
+        _clearEl: null,
         _filterInfo: null,
         _selectedTags: [],
         _invisibles: [],
@@ -204,26 +206,68 @@
             }
         },
 
+        _checkItem: function(item) {
+            item.getElementsByClassName('checkbox')[0].style.display = "inline-block";
+            item.dataset.checked = "checked";
+        },
+
+        _uncheckItem: function(item) {
+            item.dataset.checked = "";
+            item.getElementsByClassName('checkbox')[0].style.display = "none";
+        },
+
+        _onClickToItem: function(e) {
+            L.DomEvent.stop(e);
+            var li = this.element;
+            var context = this.context;
+            if (!li.dataset.checked) {
+                context._checkItem(li);
+            } else {
+                context._uncheckItem(li);
+            }
+            if (context.options.filterOnEveryClick) {
+                context.filter.call(context);
+            }
+        },
+
         _preparePopup: function(data) {
+            
+            this._tagEl.innerHTML = '';
+
             for (var i = 0; i < data.length; i++) {
-                var div = L.DomUtil.create('div', '', this._container);
-                var label = L.DomUtil.create('label', '', div);
+                var li = L.DomUtil.create('li', 'ripple', this._tagEl);
+                var checkbox = L.DomUtil.create('span', 'checkbox', li);
+                var a = L.DomUtil.create('a', '', li);
                 var text = data[i];
                 var value = data[i];
                 if (typeof text == 'object' && Object.keys(data[i]) == 2) { // key,value
                     text = data[i].name;
                     value = data[i].value;
                 }
+                
                 var checked = this._selectedTags.indexOf(value) !== -1;
-                label.innerHTML = '<input type="checkbox" ' + (checked ? 'checked="true"' : '') + 'data-value="' + value + '" class="regular-checkbox"/><label></label> ' + text;
-                label.style.width = '100%';
+                if (checked) {
+                    checkbox.style.display = "inline-block";
+                    li.dataset.checked = "checked";
+                }
+                checkbox.innerHTML= "&#10004;";
+                li.dataset.value = value;
+                a.innerText = text;
 
-                L.DomEvent.addListener(label, 'dblclick', L.DomEvent.stop);
-                L.DomEvent.addListener(label, 'click', function(e) {
+                L.DomEvent.addListener(li, 'dblclick', this._onClickToItem.bind({ context: this, element: li }));
+                L.DomEvent.addListener(li, 'click', this._onClickToItem.bind({ context: this, element: li }));
+                
+                L.DomEvent.addListener(a, 'dblclick', function(e) {
                     L.DomEvent.stop(e);
-                    var checkbox = this.getElementsByTagName("input")[0];
-                    checkbox.checked = !checkbox.checked;
-                });
+                    this.context._onClickToItem.call(this, e);
+                }.bind({ context: this, element: li }));
+
+                L.DomEvent.addListener(a, 'click', function(e) {
+                    L.DomEvent.stop(e);
+                    this.context._onClickToItem.call(this, e);
+                }.bind({ context: this, element: li }));
+
+
             }
             this._container.style.display = "block";
         },
@@ -231,18 +275,21 @@
         _clearSelections: function(e) {
             L.DomEvent.stop(e);
             this._selectedTags = [];
-            var checkboxContainer = (this._container.getElementsByTagName('div')[0]),
-                childCount = this._container.childElementCount,
-                children = this._container.children,
+            var childCount = this._tagEl.childElementCount,
+                children = this._tagEl.children,
                 childCheckbox, i;
 
             this._selectedTags = [];
 
             for (i = 0; i < childCount; i++) {
-                childCheckbox = children[i].getElementsByTagName('input')[0];
-                if (childCheckbox && childCheckbox.checked) {
-                    childCheckbox.checked = false;
+                childCheckbox = children[i];
+                if (childCheckbox) {
+                    this._uncheckItem(childCheckbox);
                 }
+            }
+
+            if (this.options.filterOnEveryClick) {
+                this.filter();
             }
         },
 
@@ -259,10 +306,6 @@
                 throw 'data is empty!';
             }
 
-            this._container.innerHTML = '<a href="#">' + this.options.clearText + '</a>';
-            var clearButton = this._container.getElementsByTagName('a')[0];
-            L.DomEvent.addListener(this._container, 'click', this._clearSelections.bind(this));
-
             if (this.options.data) {
                 if (typeof this.options.data === 'function') {
                     this._preparePopup(this.options.data());
@@ -273,26 +316,18 @@
 
         },
 
-        hide: function(accept) {
-            if (this._container.style.display == "none") {
-                return;
-            }
-
-            if (this._container) {
-                this._container.style.display = "none";
-            }
-
+        filter: function() {
             var checkboxContainer = (this._container.getElementsByTagName('div')[0]),
-                childCount = this._container.childElementCount,
-                children = this._container.children,
+                childCount = this._tagEl.childElementCount,
+                children = this._tagEl.children,
                 childCheckbox, i, j;
 
             this._selectedTags = [];
 
             for (i = 0; i < childCount; i++) {
-                childCheckbox = children[i].getElementsByTagName('input')[0];
-                if (childCheckbox && childCheckbox.checked) {
-                    this._selectedTags.push(childCheckbox.getAttribute('data-value'));
+                childCheckbox = children[i];
+                if (childCheckbox && childCheckbox.dataset.checked) {
+                    this._selectedTags.push(childCheckbox.dataset.value);
                 }
             }
 
@@ -302,7 +337,16 @@
             if (this.options.onSelectionComplete && typeof this.options.onSelectionComplete == 'function') {
                 this.options.onSelectionComplete.call(this, this._selectedTags);
             }
+        },
 
+        hide: function(accept) {
+            if (this._container.style.display == "none") {
+                return;
+            }
+            if (this._container) {
+                this._container.style.display = "none";
+            }
+            this.filter();
             this._easyButton.button.style.display = "block";
         },
 
@@ -316,8 +360,21 @@
             this._easyButton = L.easyButton(this.options.icon, this._showTagFilterPopup.bind(this)).addTo(map);
             this._container = L.DomUtil.create('div', 'tag-filter-tags-container', this._easyButton._container);
 
+            if (!L.Browser.touch) {
+                L.DomEvent.disableClickPropagation(this._container);
+                L.DomEvent.disableScrollPropagation(this._container);
+            } else {
+                L.DomEvent.on(this._container, 'click', L.DomEvent.stopPropagation);
+            }
+
             this._filterInfo = L.DomUtil.create('span', 'filter-info-box', this._easyButton._container);
             this._showFilterInfo(0);
+
+            this._clearEl = L.DomUtil.create('ul', 'header', this._container);
+            this._clearEl.innerHTML = `<li class='ripple'><a>${this.options.clearText}</a></li>`;
+
+            L.DomEvent.addListener(this._clearEl.getElementsByTagName('a')[0], 'click', this._clearSelections.bind(this));
+            L.DomEvent.addListener(this._clearEl.getElementsByTagName('li')[0], 'click', this._clearSelections.bind(this));
 
             this._tagEl = L.DomUtil.create('ul', '', this._container);
             this._map.on('dragstart click', this.hide, this);
